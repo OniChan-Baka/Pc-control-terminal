29e89e7ac495385838212b079816689f1538a12c
Onichan-Baka <famalik9876@gmail.com>
2023-01-22 21:10:17 +0500

    added a location file

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import subprocess
 
+
+def getinput():
+    return input()
+
+
+def main():
+    while True:
+        getinput()
+        if "code" in getinput:
+            subprocess.Popen("")
+
+
+if __name__ == '__main__':
+    main()
