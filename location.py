d93a5a1d9fe3a68c65cefc444b6b257484da88e7
Onichan-Baka <famalik9876@gmail.com>
2023-01-22 21:54:43 +0500

    wrote some code

--- location.py
+++ location.py
@@ -1,1 +1,7 @@
-null
+location = {
+    "vscode": r"C:\Users\famal\AppData\Local\Programs\Microsoft VS Code\Code.exe",
+    "opera": r"C:\Users\famal\AppData\Local\Programs\Opera GX\opera.exe",
+    "spotify": r"C:\Users\famal\AppData\Roaming\Spotify\Spotify.exe",
+    "notion": r"C:\Users\famal\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Notion.lnk",
+    "explorer": r"explorer"
+}
--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-import subprocess
+from location import *
+from subprocess import Popen
+from pymem import Pymem
 
 
 def getinput():
-    return input()
+    return input(">")
+
+
+def checkIfRunning(a) -> str:
+    try:
+        Pymem(a+".exe")
+    except:
+        Popen(location[a])
 
 
 def main():
     while True:
-        getinput()
-        if "code" in getinput:
-            subprocess.Popen("")
+        inp = str(getinput())
+        if "q" in inp or "exit" in inp:
+            exit()
+        if "explorer" in inp or "exp" in inp:
+            Popen(location['explorer'], shell=True)
+        if "vscode" in inp or "vs" in inp:
+            Popen(location['vscode'], shell=True)
+        if "notion" in inp or "no" in inp:
+            Popen(location['notion'], shell=True)
+        if "spotify" in inp or "sp" in inp:
+            Popen(location['spotify'], shell=True)
+        if ".code" in inp:
+            checkIfRunning("vscode")
+            checkIfRunning("opera")
+            checkIfRunning("spotify")
 
 
 if __name__ == '__main__':
